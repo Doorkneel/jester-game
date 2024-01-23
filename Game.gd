e565852ef4a57860061daa17c448ee9547d9b1fd
daniel.goosen <daniel.goosen@payat.co.za>
2024-01-23 21:19:29 +0200

    🃏 Add initial code for Game node

---
+++ Game.gd
@@ -0,0 +1,19 @@
+extends Node
+
+# Tracks which setup is currently active and pending resolution
+var current_setup = ""
+
+var round_number = 0
+var max_round_number = 7
+
+# Array of comedy scores for each round the player has played
+var comedy_score = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
