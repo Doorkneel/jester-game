9404261747c27b969cf3cad15e52724cf906f603
daniel.goosen <daniel.goosen@payat.co.za>
2024-01-23 21:14:29 +0200

    🃏 Add initial Deck node

---
+++ Deck.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# Loads cards in from the player's decklist.
+func load_deck():
+	pass
+	
+func draw():
+	pass
+
+# Randomizes order of deck.
+func shuffle():
+	pass
+
+# Adds a card to the deck.
+func add_card():
+	pass
