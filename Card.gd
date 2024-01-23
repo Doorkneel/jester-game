b620a3d59997f17bd3365dfbb1c4066c02e28977
daniel.goosen <daniel.goosen@payat.co.za>
2024-01-23 21:22:02 +0200

    🃏 Add intial code for Card node

---
+++ Card.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var card_id
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func load_card_data(card_id):
+	pass
