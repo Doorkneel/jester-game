56d8ff45468d4ce7486a157dcce77b6b2d7f6b96
Dario Trinchero <dario.trinchero@pm.me>
2024-01-26 19:58:10 +0200

    Rename scene.
    Add JSON files for cards.
    Add CardSlot scene.

---
+++ scenes/card_slot.gd
@@ -0,0 +1,11 @@
+class_name CardSlot
+extends Area2D
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_mouse_entered() -> void:
+	pass # Replace with function body.
