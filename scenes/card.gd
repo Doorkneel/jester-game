14a4be13f06cc9e07ca3e98a74cc478521521197
daniel.goosen <daniel.goosen@payat.co.za>
2024-01-26 22:02:36 +0200

    Improve card dragging

--- scenes/card.gd
+++ scenes/card.gd
@@ -1,17 +1,25 @@
 extends Area2D
 
 var card_id
-var is_mouse_hovering = false
+var is_hovering = false
+var is_selected = false
 
 func load_card_data(card_id):
 	pass
 
 func _process(delta):
-	if is_mouse_hovering == true && Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if is_selected:
 		position = get_global_mouse_position()
 
 func _on_mouse_entered():
-		is_mouse_hovering = true
+		is_hovering = true
 
-func _on_mouse_exited():
-	is_mouse_hovering = false
+func _on_mouse_exit():
+	is_hovering = false
+
+func _input(event):
+	if Input.is_action_just_pressed("click"):
+		if is_hovering:
+			is_selected = true
+	elif Input.is_action_just_released("click"):
+		is_selected = false
