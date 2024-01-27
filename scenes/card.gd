fb0114099c333fee73a1046edd56f5c20decf8b4
Dario Trinchero <dario.trinchero@pm.me>
2024-01-27 09:30:12 +0200

    Add card slots.

--- scenes/card.gd
+++ scenes/card.gd
@@ -1,16 +1,18 @@
 class_name Card
 extends Area2D
 
+signal card_picked_up(this: Card)
 signal card_released(this: Card)
 
 var card_id: String
 
 # Track card being dragged across multiple card instances
 static var card_being_dragged: Card
+var highlighted_slot: CardSlot
 
 # Current interaction
-var is_hovering = false
-var is_being_dragged = false
+var being_hovered = false
+var being_dragged = false
 
 # Where to return if let go
 var pos_in_hand: int = -1
@@ -18,8 +20,8 @@ var origin_slot: CardSlot = null
 
 # Target position & rotation for animation
 var target_position: Vector2
-var target_rotation: float
-const animation_speed: float = 0.6
+const slide_speed: float = 0.6
+const snap_strength: float = 0.7
 
 func load_card_data(id) -> void:
 	card_id = id
@@ -27,41 +29,51 @@ func load_card_data(id) -> void:
 
 func _ready() -> void:
 	target_position = position
-	target_rotation = rotation
 
 func _process(_delta) -> void:
-	if is_being_dragged:
-		position = get_global_mouse_position()
-	else:
-		# move towards target position
-		var pos_delta = target_position - position
-		if pos_delta.length() < animation_speed: position = target_position
-		else: position += pos_delta * animation_speed
+	if being_dragged:
+		if highlighted_slot:
+			target_position = snap_strength * highlighted_slot.global_position + (1 - snap_strength) * get_global_mouse_position()
+			rotation = highlighted_slot.global_rotation
+		else:
+			target_position = get_global_mouse_position()
+			rotation = 0
 		
-		# move towards target rotation
-		var rot_delta = target_rotation - rotation
-		if abs(rot_delta) < animation_speed: rotation = target_rotation
-		else: rotation += rot_delta * animation_speed
+	# move towards target position
+	var pos_delta = target_position - position
+	if pos_delta.length() < slide_speed: position = target_position
+	else: position += pos_delta * slide_speed
 
 func _on_mouse_entered() -> void:
-	is_hovering = true
+	being_hovered = true
 
 func _on_mouse_exited() -> void:
-	is_hovering = false
+	being_hovered = false
 
 func _input(_event) -> void:
 	if Input.is_action_just_pressed("click"):
-		if is_hovering and not card_being_dragged:
-			is_being_dragged = true
+		if being_hovered and not card_being_dragged:
+			being_dragged = true
+			card_picked_up.emit(self)
 			card_being_dragged = self
-			rotation = 0
 	elif Input.is_action_just_released("click"):
-		if is_being_dragged:
-			is_being_dragged = false
+		if being_dragged:
+			being_dragged = false
 			card_released.emit(self)
 			card_being_dragged = null
+			
+			if highlighted_slot:
+				play_to_slot(highlighted_slot)
+			else:
+				# TODO add logic to return to hand using 'pos_in_hand'
+				pass
 
-func snap_to_slot(slot: CardSlot) -> void:
-	target_position = slot.position
-	target_rotation = slot.rotation
+func _on_slot_hovered(slot: CardSlot) -> void:
+	# TODO do some logic to check whether card can be placed here
+	highlighted_slot = slot
+
+func play_to_slot(slot: CardSlot) -> void:
+	target_position = slot.global_position
+	rotation = slot.global_rotation
+	slot.contents.append(self)
 	origin_slot = slot
--- scenes/game.gd
+++ scenes/game.gd
@@ -4,17 +4,17 @@ extends Node
 var round_number: int = 0
 var max_round_number: int = 7
 
-var highlighted_slot: CardSlot
+@onready var slots = get_tree().get_nodes_in_group("slots") as Array[CardSlot]
 
 func _ready() -> void:
-	for slot in get_tree().get_nodes_in_group("slots") as Array[CardSlot]:
-		slot.connect("slot_highlighted", self._on_card_slot_highlighted)
 	for card in get_tree().get_nodes_in_group("cards") as Array[Card]:
 		card.connect("card_released", self._on_card_released)
-
-func _on_card_slot_highlighted(slot: CardSlot) -> void:
-	highlighted_slot = slot
+		card.connect("card_picked_up", self._on_card_picked_up)
 
 func _on_card_released(card: Card) -> void:
-	if highlighted_slot:
-		card.snap_to_slot(highlighted_slot)
+	for slot in slots:
+		slot.disconnect("slot_hovered", card._on_slot_hovered)
+
+func _on_card_picked_up(card: Card) -> void:
+	for slot in slots:
+		slot.connect("slot_hovered", card._on_slot_hovered)
